3967e7a6dbb1ee077ea78c0fc1a671b6c3a72c87
Stephen Tim <571268@qq.com>
2025-03-21 01:25:38 +0800

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Stephen Tim on 2025/3/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by Stephen Tim on 2025/3/21.
+//
+
+import Testing
+
+struct CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
